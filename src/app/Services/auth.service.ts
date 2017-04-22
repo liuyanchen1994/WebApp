d853c94ee80a1d29a177f33673dc524d872773af
NilTor <zpty@outlook.com>
2017-04-22 18:09:13 +0800

    just save update

---
+++ src/app/Services/auth.service.ts
@@ -0,0 +1,33 @@
+import { Injectable } from '@angular/core';
+import { Http, Response } from '@angular/http';
+import { Observable } from 'rxjs/Observable';
+import 'rxjs/add/operator/map';
+import 'rxjs/add/operator/catch';
+import 'rxjs/add/operator/switchMap';
+
+
+@Injectable()
+export class AuthService {
+
+  oauthUrl = 'https://login.microsoftonline.com/common/oauth2/v2.0/authorize';
+  clientId = '6c76318e-9729-45b6-abf2-89440f6fc536';
+  redirectUri = 'http://localhost:4200/login';
+  constructor(private http: Http) { }
+
+  login() {
+    const url = this.oauthUrl
+      + '?client_id=' + this.clientId
+      + '&response_type=id_token+token&redirect_uri'
+      + this.redirectUri
+      + '&scope=openid%20https%3A%2F%2Fgraph.microsoft.com%2Fmail.read'
+      + '&response_mode=fragment'
+      + '&state=MSDev';
+
+    return this.http.get(url)
+      .map((response: Response) => {
+        let re = response.json();
+        console.log(re);
+
+      });
+  }
+}
