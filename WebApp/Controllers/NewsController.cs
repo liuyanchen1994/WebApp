61406cf922009fadaf9485edbfe494f69493b247
NilTor <chenxuesong@cisscool.com>
2017-07-04 18:39:11 +0800

    rebuild

--- WebApp/Controllers/NewsController.cs
+++ WebApp/Controllers/NewsController.cs
@@ -5,14 +5,33 @@ using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using WebApp.Models;
+using System.Net.Http;
+using Newtonsoft.Json;
+using WebApp.Models.Common;
+using WebApp.Models.Entity;
+using WebApp.Models.ViewModels;
 
 namespace WebApp.Controllers
 {
     public class NewsController : Controller
     {
-        public IActionResult Index()
+        public async Task<IActionResult> Index ()
         {
-            return View();
+
+            HttpClient client = new HttpClient
+            {
+                BaseAddress = new Uri("https://api.msdev.cc/")
+            };
+            var re = await client.GetStringAsync("api/BingNews/PageList");
+            var jsonRe = JsonConvert.DeserializeObject<ReturnJson<IEnumerable<BingNews>>>(re);
+
+            var data = new NewsViewModels();
+            if (jsonRe.ErrorCode == 0)
+            {
+                data.BingNewsList = jsonRe.Data.ToList();
+            }
+            return View(data);
         }
+
     }
 }
---
+++ WebApp/Models/ViewModels/NewsViewModels.cs
@@ -0,0 +1,16 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using WebApp.Models.Entity;
+
+namespace WebApp.Models.ViewModels
+{
+    public class NewsViewModels
+    {
+
+        public List<BingNews> BingNewsList { get; set; }
+
+
+    }
+}
