50ed5d4ed97db7476e510d4e32d4d2d1b49f1676
NilTor <chenxuesong@cisscool.com>
2017-07-06 17:52:53 +0800

    add db support

---
+++ WebApp/DB/Entity/AspNetUserRoles.cs
@@ -0,0 +1,14 @@
+using System;
+using System.Collections.Generic;
+
+namespace WebApp.DB
+{
+    public partial class AspNetUserRoles
+    {
+        public string UserId { get; set; }
+        public string RoleId { get; set; }
+
+        public AspNetRoles Role { get; set; }
+        public AspNetUsers User { get; set; }
+    }
+}
--- WebApp/DB/Entity/BingNews.cs
+++ WebApp/DB/Entity/BingNews.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 
-namespace WebApp.Models.Entity
+namespace WebApp.DB
 {
     public class BingNews : Model
     {
