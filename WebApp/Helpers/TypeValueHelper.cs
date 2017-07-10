0ced93b2fc4e91e1e83ccee93717907a9cb4bc95
NilTor <zpty@outlook.com>
2017-07-10 23:00:12 +0800

    download page.show resource info

---
+++ WebApp/Helpers/TypeValueHelper.cs
@@ -0,0 +1,16 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WebApp.Helpers
+{
+    public static class TypeValueHelper
+    {
+
+        public static string[] Language = { "中文", "英文" };
+
+        public static string[] DownloadType = { "文档", "视频", "软件", "代码" };
+
+    }
+}
