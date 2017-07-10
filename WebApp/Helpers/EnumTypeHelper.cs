3dda66126741b03f1f28d253d3c38b62f2695bdf
NilTor <zpty@outlook.com>
2017-07-10 23:00:12 +0800

    download page.show resource info

---
+++ WebApp/Helpers/EnumTypeHelper.cs
@@ -0,0 +1,24 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WebApp.Helpers
+{
+    public class EnumTypeHelper
+    {
+        public enum LanguageType
+        {
+            Cn,
+            En
+        }
+
+        public enum ReourceType
+        {
+            Doucment,
+            Video,
+            Software,
+            Code
+        }
+    }
+}
