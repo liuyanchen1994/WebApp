0c770a934bf7403a1f2203c89b73357809eb09ff
NilTor <zpty@outlook.com>
2017-07-29 14:44:23 +0800

    add baidu statistics

---
+++ WebApp/wwwroot/js/baidu.js
@@ -0,0 +1,8 @@
+//百度统计代码
+var _hmt = _hmt || [];
+(function () {
+    var hm = document.createElement("script");
+    hm.src = "https://hm.baidu.com/hm.js?14364b16f9e46d91a5a7685b46c82001";
+    var s = document.getElementsByTagName("script")[0];
+    s.parentNode.insertBefore(hm, s);
+})();
