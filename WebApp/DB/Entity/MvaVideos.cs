75c058d550137e45adf60d933a0eac9c20a4ca70
NilTor <zpty@outlook.com>
2017-10-04 12:23:56 +0800

    fix rssnewsdetail ,footer qq_group

--- WebApp/DB/Entity/MvaVideos.cs
+++ WebApp/DB/Entity/MvaVideos.cs
@@ -31,5 +31,6 @@ namespace WebApp.DB
         public string Title { get; set; }
         public bool IsRecommend { get; set; }
         public DateTime UpdatedTime { get; set; }
+        public int? Views { get; set; }
     }
 }
