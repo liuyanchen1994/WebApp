80cb4ccf1230c1be43d321b6caadc4e1f2b4cc2e
NilTor <geethin@outlook.com>
2017-11-14 13:25:58 +0800

    daily update

---
+++ WebApp/DB/Entity/C9Videos.cs
@@ -0,0 +1,31 @@
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+
+namespace WebApp.DB
+{
+    public partial class C9Videos
+    {
+        public Guid Id { get; set; }
+        public string Author { get; set; }
+        public DateTime CreatedTime { get; set; }
+        public string Description { get; set; }
+        public string Duration { get; set; }
+        public string Language { get; set; }
+        public string Mp3Url { get; set; }
+        public string Mp4HigUrl { get; set; }
+        public string Mp4LowUrl { get; set; }
+        public string Mp4MidUrl { get; set; }
+        public string SeriesTitle { get; set; }
+        public string SeriesTitleUrl { get; set; }
+        [MaxLength(64)]
+        public string SeriesType { get; set; }
+        public string SourceUrl { get; set; }
+        public int? Status { get; set; }
+        public string Tags { get; set; }
+        public string ThumbnailUrl { get; set; }
+        public string Title { get; set; }
+        public DateTime UpdatedTime { get; set; }
+        public int? Views { get; set; }
+    }
+}
